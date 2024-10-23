5fb749eabd63eb3f550e4bc854bd154ca09ade9c
Yu-Wei Chang <hejyuwei@gmail.com>
2024-10-23 14:38:12 +0200

    Create script.m

---
+++ NNClassifyPyschiatricDisorders/script.m
@@ -0,0 +1,4 @@
+% Yuxin, put your code here
+% This script will load brain atlas and the data.
+% This script will train the classifiers for the 3 combinations of the 3 groups
+% This script will fix the split across all the combinations
